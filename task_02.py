d8dd4f86081d3c468db89315389a6be20e33cb4f
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-18 12:20:17 -0400

    changed continue to else statement

--- task_02.py
+++ task_02.py
@@ -7,13 +7,15 @@ INPUT_NUM = 0
 FACTORIAL = 1
 PRODUCT = 1
 
-while VALID is not True:
+while not VALID:
     INPUT_NUM = int(raw_input("Enter a number >> "))
     if INPUT_NUM > 0:
         break
     if INPUT_NUM < 0:
         print "Invalid number: Please enter a number greater than zero."
-        continue
+    else:
+        VALID = False
+# continue
 
 while FACTORIAL <= INPUT_NUM:
     PRODUCT = PRODUCT * FACTORIAL
