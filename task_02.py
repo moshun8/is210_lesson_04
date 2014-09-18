f2ed76b0c3db222f08ec2dd7727063b5d11c9ea7
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-18 13:00:16 -0400

    changed break to = True

--- task_02.py
+++ task_02.py
@@ -10,12 +10,11 @@ PRODUCT = 1
 while not VALID:
     INPUT_NUM = int(raw_input("Enter a number >> "))
     if INPUT_NUM > 0:
-        break
-    if INPUT_NUM < 0:
+        VALID = True
+    elif INPUT_NUM < 0:
         print "Invalid number: Please enter a number greater than zero."
     else:
         VALID = False
-# continue
 
 while FACTORIAL <= INPUT_NUM:
     PRODUCT = PRODUCT * FACTORIAL
