961c20d7b5012eebb119163e578454cc6624ea1e
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-18 08:52:10 -0400

    cleaning up pylint errors task 1

--- task_01.py
+++ task_01.py
@@ -12,23 +12,25 @@ MINIMUM_WORDS = 10000.0
 AVERAGE_WORDS = 0.0
 NUM_CRISPIAN = 0.0
 
-LINES = POEM.split('\n') # Splits POEM into lines
+LINES = POEM.split('\n')  # Splits POEM into lines
 TOTAL_WORDS = 0.0
 TIMES_CRISPIAN = 0.0
 
 for LINE in LINES:
-    WORDS = LINE.split() # Splits lines into words
-    SIZE = len(WORDS) # Finds number of words in a line
-    TOTAL_WORDS += SIZE # Adds up number of words in whole doc
+    WORDS = LINE.split()  # Splits lines into words
+    SIZE = len(WORDS)  # Finds number of words in a line
+    TOTAL_WORDS += SIZE  # Adds up number of words in whole doc
     AVERAGE_WORDS = float(TOTAL_WORDS)/float(len(LINES))
-    if SIZE > MAXIMUM_WORDS: MAXIMUM_WORDS = SIZE
-    if SIZE < MINIMUM_WORDS: MINIMUM_WORDS = SIZE
-    if "Crispian" in LINE: NUM_CRISPIAN += 1
+    if SIZE > MAXIMUM_WORDS:
+        MAXIMUM_WORDS = SIZE
+    if SIZE < MINIMUM_WORDS:
+        MINIMUM_WORDS = SIZE
+    if "Crispian" in LINE:
+        NUM_CRISPIAN += 1
 
 print MAXIMUM_WORDS, MINIMUM_WORDS, TOTAL_WORDS, AVERAGE_WORDS, NUM_CRISPIAN
 
 
 # Need a count variable and accumulator variable
 # if MAXIMUM_WORDS > accumulator:
-#	MAXIMUM_WORDS = accumulator"""
-
+# MAXIMUM_WORDS = accumulator"""
