2847b5176ba60e9a8449198ae1ee6df469d97684
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-19 10:50:04 -0400

    pylint errors, deleted old code I saved with #

--- task_03.py
+++ task_03.py
@@ -11,28 +11,13 @@ NUM_GUESS = 3
 # reread instructions, everything should depend on Access
 while not ACCESS:
     GUESS = raw_input(
-    "What is your password ({0} attempts left)? ".format(NUM_GUESS))
+        "What is your password ({0} attempts left)? ".format(NUM_GUESS))
     print GUESS
     if GUESS == PASSWORD:
         print "Access granted!"
-        ACCESS = True    
-    else: 
-	    NUM_GUESS -= 1
-            if NUM_GUESS == 0:
-                print "Access denied. Please try again later."
-                break
-
-#while NUM_GUESS > 0:
-#    GUESS = raw_input(
-#    "What is your password ({0} attempts left)? ").format(NUM_GUESS)
-#    print GUESS
-#    NUM_GUESS -= 1
-#    if GUESS == PASSWORD:
-#        print "Access granted!"
-#        ACCESS = True
-#        break
-#	 if GUESS != PASSWORD:
-#            ACCESS = False
-#    else:
-#        print "Access denied. Please try again later."
-#        break
+        ACCESS = True
+    else:
+        NUM_GUESS -= 1
+        if NUM_GUESS == 0:
+            print "Access denied. Please try again later."
+            break
